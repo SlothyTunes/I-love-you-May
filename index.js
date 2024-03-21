1c6fed135e8a805f0596211cad0db12cee8e0d08
Esteban Barrón <ebarronteodoro@hotmail.com>
2024-03-21 16:41:42 -0500

    Subida 2

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
-onload = () => {
-    const c = setTimeout(() => {
-        document.body.classList.remove("not-loaded");
-        clearTimeout(c);
-        playAudio();
-    }, 2000);
-};
-
 const audio = document.querySelector("audio");
-const button = document.querySelector("#custom-button");
-const volume_1 = document.querySelector(".unmute");
-const volume_2 = document.querySelector(".mute");
+const overlay = document.querySelector(".overlay");
 
-function playAudio() {
-  
+function play() {
+  document.body.classList.remove("not-loaded");
+  overlay.classList.add("over-off");
   audio.play();
   audio.volume = 0.035;
+  setTimeout(() => {
+    overlay.style.display = "none";
+  }, 1000);
 }
